e3a0bd1a9746db0796acbff1d84fdabea4587421
EMQ-YangM <yangm@emqx.io>
2021-11-03 10:53:56 +0800

    add Eventlog

--- src/Control/Effect/IOClasses.hs
+++ src/Control/Effect/IOClasses.hs
@@ -1,1 +1,10 @@
+{-# LANGUAGE GADTs #-}
 module Control.Effect.IOClasses where
+
+import           Control.Algebra
+-- import Control.Monad.Class.MonadAsync
+import           Control.Monad.Class.MonadEventlog
+
+data Eventlog m a where
+  TraceEventIO :: String -> Eventlog m ()
+  TraceMarkerIO :: String -> Eventlog m ()
