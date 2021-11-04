4b3f83db30eb3b27d6d38fd8cb35ee8b1c99fb49
EMQ-YangM <yangm@emqx.io>
2021-11-04 10:05:15 +0800

    finish

--- src/Control/Effect/IOClasses.hs
+++ src/Control/Effect/IOClasses.hs
@@ -1,10 +1,29 @@
-{-# LANGUAGE GADTs #-}
-module Control.Effect.IOClasses where
+{-# LANGUAGE AllowAmbiguousTypes   #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeOperators         #-}
+
+module Control.Effect.IOClasses
+  ( Algebra(..)
+  , module E
+  ) where
 
 import           Control.Algebra
--- import Control.Monad.Class.MonadAsync
-import           Control.Monad.Class.MonadEventlog
+import           Control.Carrier.Lift
+import           Control.Monad.Class.MonadAsync    as E
+import           Control.Monad.Class.MonadEventlog as E
+import           Control.Monad.Class.MonadFork     as E
+import           Control.Monad.Class.MonadST       as E
+import           Control.Monad.Class.MonadSTM      as E
+import           Control.Monad.Class.MonadSay      as E
+import           Control.Monad.Class.MonadThrow    as E
+import           Control.Monad.Class.MonadTime     as E
+import           Control.Monad.Class.MonadTimer    as E
+import           Control.Monad.IOSim               as E
+import           Control.Monad.ST.Lazy             as E
 
-data Eventlog m a where
-  TraceEventIO :: String -> Eventlog m ()
-  TraceMarkerIO :: String -> Eventlog m ()
+instance Algebra (Lift (IOSim s)) (IOSim s) where
+  alg hdl (LiftWith with) = with hdl
